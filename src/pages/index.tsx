642d0dca36d6f390ab75f1ade6364a6a426a4e5e
Pedro Ivo Bueno Sartorio <pedroivobu@gmail.com>
2024-04-26 12:32:21 -0300

    Update to use Remote backend

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,3 @@
-
 import React, { useState, useEffect } from "react";
 import {
   Typography,
@@ -11,96 +10,30 @@ import {
   TableBody,
   Paper,
 } from "@mui/material";
-import hmacSHA256 from "crypto-js/hmac-sha256";
-import Base64 from "crypto-js/enc-base64";
 import axios from "axios";
 import SearchInput from "../components/SearchInput";
 
 const MainPage: React.FC = () => {
   const [clientName, setClientName] = useState<string>("");
   const [clients, setClients] = useState<any[]>([]);
-//   const [errorMessage, setErrorMessage] = useState<string>("");
 
   useEffect(() => {
     fetchClients();
   }, []);
 
   const fetchClients = () => {
-    var timestamp = Math.floor(Date.now() / 1000).toString();
-
-    // Function to generate the Signature
-    function generateSignature() {
-      const method = "get"; // Assuming 'get' method for example
-      // const timestamp = Date.now().toString(); // Current timestamp
-
-      // Concatenate the uppercase method with the timestamp
-      const concatString = method.toLowerCase() + timestamp;
-      const secretKey = "senha";
-
-      // Hash the concatenated string using SHA-256
-      const hash = Base64.stringify(hmacSHA256(concatString, secretKey));
-
-      return hash;
-    }
-
-    // Function to make the request
-    function makeRequest() {
-      const signature = generateSignature();
-      console.log("signature:", signature);
-
-      axios
-        .get(
-          `https://cors-anywhere.herokuapp.com/http://idealsoftexportaweb.eastus.cloudapp.azure.com:60500/auth/?serie=HIEAPA-600759-ROCT&codfilial=1`,
-          {
-            headers: {
-              "cache-control": "no-cache",
-              Signature: "-1",
-            },
-          }
-        )
-        .then((authResponse) => {
-          // Extract the token from the response data
-          const token = authResponse.data.dados.token;
-          console.log("passei por aqui");
-
-          // Use the obtained token to make the request for client data
-          axios
-            .get(
-              `https://cors-anywhere.herokuapp.com/http://idealsoftexportaweb.eastus.cloudapp.azure.com:60500/clientes/9`,
-              {
-                headers: {
-                  "cache-control": "no-cache",
-                  Signature: signature,
-                  CodFilial: "1",
-                  Authorization: `Token ${token}`, // Include the token in the Authorization header
-                  Timestamp: timestamp, // Include the timestamp in the headers
-                },
-              }
-            )
-            .then((clientResponse) => {
-              // Log the client data to the console
-              setClients(clientResponse.data.dados);
-              console.log(JSON.stringify(clientResponse.data));
-            })
-            .catch((error) => {
-              // Log any errors encountered during the request for client data
-              console.log("Error fetching client data:", error);
-            });
-        })
-        .catch((error) => {
-          // Log any errors encountered during the request for the authentication token
-          console.log("Error fetching authentication token:", error);
-        });
-    }
-
-    // Call the function to make the request
-    makeRequest();
+    axios.get("https://acepil-server.onrender.com/api/")
+      .then((response) => {
+        setClients(response.data);
+      })
+      .catch((error) => {
+        console.error("Error fetching client data:", error);
+      });
   };
 
   const handleSearch = () => {
     if (!clientName.trim()) {
-        fetchClients();
-    //   setErrorMessage("Please enter a client name.");
+      fetchClients();
       return;
     }
 
@@ -126,12 +59,6 @@ const MainPage: React.FC = () => {
           placeholder={"Buscar cliente"}
         />
 
-        {/* {errorMessage && (
-          <Typography variant="body1" color="error" align="center" gutterBottom>
-            {errorMessage}
-          </Typography>
-        )} */}
-
         <TableContainer component={Paper}>
           <Table>
             <TableHead>
@@ -139,7 +66,6 @@ const MainPage: React.FC = () => {
                 <TableCell>Nome do Cliente</TableCell>
                 <TableCell>CPF/CNPJ</TableCell>
                 <TableCell>Endereço</TableCell>
-                {/* Add more table headers as needed */}
               </TableRow>
             </TableHead>
             <TableBody>
@@ -148,7 +74,6 @@ const MainPage: React.FC = () => {
                   <TableCell>{client.nome}</TableCell>
                   <TableCell>{client.cpfCnpj}</TableCell>
                   <TableCell>{`${client.endereco}, ${client.numero}, ${client.bairro}, ${client.cidade} - ${client.uf}`}</TableCell>
-                  {/* Add more table cells based on the client object properties */}
                 </TableRow>
               ))}
             </TableBody>
--- src/pages/products.tsx
+++ src/pages/products.tsx
@@ -1,5 +1,3 @@
-
-// puxar nome do producte e valor do pedido
 import React, { useState, useEffect } from "react";
 import {
   Typography,
@@ -12,96 +10,30 @@ import {
   TableBody,
   Paper,
 } from "@mui/material";
-import hmacSHA256 from "crypto-js/hmac-sha256";
-import Base64 from "crypto-js/enc-base64";
 import axios from "axios";
 import SearchInput from "../components/SearchInput";
 
 const Product: React.FC = () => {
   const [productName, setProductName] = useState<string>("");
   const [products, setProducts] = useState<any[]>([]);
-//   const [errorMessage, setErrorMessage] = useState<string>("");
 
   useEffect(() => {
     fetchProducts();
   }, []);
 
   const fetchProducts = () => {
-    var timestamp = Math.floor(Date.now() / 1000).toString();
-
-    // Function to generate the Signature
-    function generateSignature() {
-      const method = "get"; // Assuming 'get' method for example
-      // const timestamp = Date.now().toString(); // Current timestamp
-
-      // Concatenate the uppercase method with the timestamp
-      const concatString = method.toLowerCase() + timestamp;
-      const secretKey = "senha";
-
-      // Hash the concatenated string using SHA-256
-      const hash = Base64.stringify(hmacSHA256(concatString, secretKey));
-
-      return hash;
-    }
-
-    // Function to make the request
-    function makeRequest() {
-      const signature = generateSignature();
-      console.log("signature:", signature);
-
-      axios
-        .get(
-          `https://cors-anywhere.herokuapp.com/http://idealsoftexportaweb.eastus.cloudapp.azure.com:60500/auth/?serie=HIEAPA-600759-ROCT&codfilial=1`,
-          {
-            headers: {
-              "cache-control": "no-cache",
-              Signature: "-1",
-            },
-          }
-        )
-        .then((authResponse) => {
-          // Extract the token from the response data
-          const token = authResponse.data.dados.token;
-          console.log("passei por aqui");
-
-          // Use the obtained token to make the request for product data
-          axios
-            .get(
-              `https://cors-anywhere.herokuapp.com/http://idealsoftexportaweb.eastus.cloudapp.azure.com:60500/produtos/1`,
-              {
-                headers: {
-                  "cache-control": "no-cache",
-                  Signature: signature,
-                  CodFilial: "1",
-                  Authorization: `Token ${token}`, // Include the token in the Authorization header
-                  Timestamp: timestamp, // Include the timestamp in the headers
-                },
-              }
-            )
-            .then((productResponse) => {
-              // Log the product data to the console
-              setProducts(productResponse.data.dados);
-              console.log(JSON.stringify(productResponse.data));
-            })
-            .catch((error) => {
-              // Log any errors encountered during the request for product data
-              console.log("Error fetching product data:", error);
-            });
-        })
-        .catch((error) => {
-          // Log any errors encountered during the request for the authentication token
-          console.log("Error fetching authentication token:", error);
-        });
-    }
-
-    // Call the function to make the request
-    makeRequest();
+    axios.get("https://acepil-server.onrender.com/api/products")
+      .then((response) => {
+        setProducts(response.data);
+      })
+      .catch((error) => {
+        console.error("Error fetching product data:", error);
+      });
   };
 
   const handleSearch = () => {
-    if (!productName) {
-        fetchProducts();
-    //   setErrorMessage("Please enter a product name.");
+    if (!productName.trim()) {
+      fetchProducts();
       return;
     }
 
@@ -116,7 +48,7 @@ const Product: React.FC = () => {
   return (
     <React.Fragment>
       <Container maxWidth="xl">
-      <Typography variant="h4" align="left" gutterBottom>
+        <Typography variant="h4" align="left" gutterBottom>
           Buscar produtos
         </Typography>
 
@@ -127,20 +59,13 @@ const Product: React.FC = () => {
           placeholder={"Buscar produto"}
         />
 
-        {/* {errorMessage && (
-          <Typography variant="body1" color="error" align="center" gutterBottom>
-            {errorMessage}
-          </Typography>
-        )} */}
-
         <TableContainer component={Paper}>
           <Table>
             <TableHead>
               <TableRow>
                 <TableCell>Nome do Produto</TableCell>
-                <TableCell>Codigo</TableCell>
+                <TableCell>Código</TableCell>
                 <TableCell>Preço</TableCell>
-                {/* Add more table headers as needed */}
               </TableRow>
             </TableHead>
             <TableBody>
@@ -148,8 +73,7 @@ const Product: React.FC = () => {
                 <TableRow key={index}>
                   <TableCell>{product.nome}</TableCell>
                   <TableCell>{product.codigo}</TableCell>
-                  <TableCell>{`R$: ${product.precos[0].preco},00`}</TableCell>
-                  {/* Add more table cells based on the product object properties */}
+                  <TableCell>{`R$ ${product.precos[0].preco},00`}</TableCell>
                 </TableRow>
               ))}
             </TableBody>
